eaaac06012eca823d567f9070be6917a7854a1b0
Rohit Dasari <Rohit Dasari@COGNINE-L82>
2021-05-09 22:25:49 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Humanizer;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(TimeSpan.FromMilliseconds(1234557464).Humanize());
+            Console.WriteLine(TimeSpan.FromMilliseconds(847362426283).Humanize(3));
+            Console.WriteLine(TimeSpan.FromMilliseconds(6353426283).Humanize(4));
+        }
+    }
+}
