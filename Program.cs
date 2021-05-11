6a1c203353bf343a0ceefe2e69f1b6c6588cb8ee
Rohitprabha <rohitprabha1234@gmail.com>
2021-05-11 16:05:57 +0530

    change in Humanizer

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace ConsoleApp2
         static void Main(string[] args)
         {
             Console.WriteLine(TimeSpan.FromMilliseconds(1234557464).Humanize());
-            Console.WriteLine(TimeSpan.FromMilliseconds(847362426283).Humanize(3));
+            Console.WriteLine(TimeSpan.FromMilliseconds(847362426283).Humanize(1));
             Console.WriteLine(TimeSpan.FromMilliseconds(6353426283).Humanize(4));
         }
     }
